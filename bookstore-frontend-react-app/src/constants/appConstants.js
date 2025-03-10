373432911a95fc1a2fc22a81223bd6325c2802c4
Pravardhan <pravardhan.mannem@gmail.com>
2025-03-11 04:57:06 +0530

    Added K8 deployment yaml file

--- bookstore-frontend-react-app/src/constants/appConstants.js
+++ bookstore-frontend-react-app/src/constants/appConstants.js
@@ -1,4 +1,4 @@
-//export const BACKEND_API_GATEWAY_URL = 'http://localhost:8765';
-export const BACKEND_API_GATEWAY_URL = `http://${process.env.BOOKSTORE_API_GATEWAY_SERVICE || 'localhost'}:8765`;
+export const BACKEND_API_GATEWAY_URL = 'http://135.235.209.44:8765';
+//export const BACKEND_API_GATEWAY_URL = `http://${process.env.BOOKSTORE_API_GATEWAY_SERVICE || 'localhost'}:8765`;
 export const APP_CLIENT_ID = '93ed453e-b7ac-4192-a6d4-c45fae0d99ac';
 export const APP_CLIENT_SECRET = 'client.devd123';
